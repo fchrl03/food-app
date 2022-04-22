26346a8e66024f521c9608bd6a62537e08242be9
Fachrul <fachrulfatahillah@gmail.com>
2022-04-22 22:52:45 +0700

    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h2>
+        <strong>Delicious Food Menu, </strong><br />
+        Your Gadget
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
+
+<style></style>
