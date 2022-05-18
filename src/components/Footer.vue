989f03bb10fa4452369116ce86025d83468ae6d0
Fachrul <fachrulfatahillah@gmail.com>
2022-05-18 20:56:18 +0700

    Add Card Product And Search

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="mt-5 mb-3">
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">2022 Copyright Fachrul Fatahillah</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
